c34737f8386cf5dcdd3b63459a8d3a4393102b9d
khethelogp <khethelogp@gmail.com>
2022-06-03 13:48:34 +0200

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ServicesComponent } from './components/services/services.component';
+import { CtaComponent } from './components/cta/cta.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PortfolioDetailsComponent } from './components/portfolio/portfolio-details/portfolio-details.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    TopbarComponent,
+    HeroComponent,
+    AboutComponent,
+    WhyUsComponent,
+    ClientsComponent,
+    ServicesComponent,
+    CtaComponent,
+    PortfolioComponent,
+    PortfolioDetailsComponent,
+    PricingComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
